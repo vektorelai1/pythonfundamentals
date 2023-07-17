d95c0d4acccc6b2d616b58e97137ce4e18932b3f
vektorelai1 <139742244+vektorelai1@users.noreply.github.com>
2023-07-17 12:26:51 +0300

    17072023

--- ilk.py
+++ ilk.py
@@ -1,1 +1,12 @@
 print("Merhaba")
+liste = ["ibrahim",
+         "pehlivan",
+         "seher","abdullah",
+         "yunus",
+         "fatih","murat" ,"cagatay"]
+import os
+filename="deneme.ipynb"
+for item in liste:
+    if not os.path.exists(os.path.join("Egzersizler",item)):
+        os.mkdir(os.path.join("Egzersizler",item))
+    open(os.path.join("Egzersizler",item,filename),"w")
