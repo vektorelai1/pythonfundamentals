d4b445f880e5ec6c323b753c74d369c00d7daba3
İbrahim EDİZ <edizdesign@gmail.com>
2023-07-19 09:34:16 +0000

    19072023

--- ilk.py
+++ ilk.py
@@ -4,8 +4,14 @@ liste = ["ibrahim",
          "yunus",
          "fatih","murat" ,"cagatay"]
 import os
+import shutil
 filename="01_pandas.ipynb"
 for item in liste:
     if not os.path.exists(os.path.join("Egzersizler",item)):
         os.mkdir(os.path.join("Egzersizler",item))
+    if not os.path.exists(os.path.join("Egzersizler",item,"data")):
+        os.mkdir(os.path.join("Egzersizler",item,"data"))
+    srcPath = "/workspace/pythonfundamentals/Dokumanlar/04_Libraries/data/pokemon.csv"
+    destPath = fr"/workspace/pythonfundamentals/Egzersizler/{item}/data/pokemon.csv"
+    shutil.copy(srcPath,destPath)
     open(os.path.join("Egzersizler",item,filename),"w")
