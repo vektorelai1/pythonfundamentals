de3dee5f6b862e83c403f849e632c0b313e88ce8
vektorelai1 <139742244+vektorelai1@users.noreply.github.com>
2023-07-19 11:25:52 +0300

    19072023_2

--- ilk.py
+++ ilk.py
@@ -1,11 +1,10 @@
-print("Merhaba")
 liste = ["ibrahim",
          "pehlivan",
          "seher","abdullah",
          "yunus",
          "fatih","murat" ,"cagatay"]
 import os
-filename="egzersiz1.ipynb"
+filename="01_pandas.ipynb"
 for item in liste:
     if not os.path.exists(os.path.join("Egzersizler",item)):
         os.mkdir(os.path.join("Egzersizler",item))
